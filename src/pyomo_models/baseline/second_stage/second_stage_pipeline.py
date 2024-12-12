76d5884a6734efde795fa4d2eef9040fa99061be
Tomasini Luca <luca.tomasini@heig-vd.ch>
2024-12-12 15:12:55 +0100

    Remove sets

--- src/pyomo_models/baseline/second_stage/second_stage_pipeline.py
+++ src/pyomo_models/baseline/second_stage/second_stage_pipeline.py
@@ -92,7 +92,24 @@ class BaselineSecondStage(BaseLineInput):
     
     def generate_model(self):
         self.model: pyo.AbstractModel = pyo.AbstractModel()
-        self.model = baseline_sets(self.model)
+        self.model.T = pyo.Set()
+        self.model.H = pyo.Set()
+        self.model.B = pyo.Set()
+        # index gathering the state per basin and the hydro powerplants
+        self.model.S_B = pyo.Set(self.model.B)
+        self.model.S_H = pyo.Set(self.model.H)
+        # index gathering the state of every basin and hydro powerplants
+        self.model.BS = pyo.Set(dimen=2, initialize=lambda model: [(b, s_b) for b in model.B for s_b in model.S_B[b]])
+        self.model.HS = pyo.Set(dimen=2, initialize=lambda model: [(h, s_h) for h in model.H for s_h in model.S_H[h]])
+        
+        self.model.S_Q = pyo.Set(self.model.HS)
+        self.model.HQS = pyo.Set(
+            dimen=3, initialize=lambda model: [(h, s_h, s_q) for (h, s_h) in model.HS for s_q in model.S_Q[h, s_h]])
+    
+    # index (gathering h, b, s_h, s_b) to make the correspondence between the state of basin and hydro powerplants
+        self.model.B_H = pyo.Set(self.model.H)
+        self.model.SB_H = pyo.Set(self.model.HS)
+        # self.model = baseline_sets(self.model)
         self.model = baseline_parameters(self.model)
         self.model = baseline_variables(self.model)
         
